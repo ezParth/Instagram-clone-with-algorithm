ee643a9ea0e38301d3b23c6f77b1dc0159816429
ezParth <parthsingh55m@gmail.com>
2024-08-17 17:09:21 +0530

    added post schema and post controller

--- api/controllers/Post.controller.js
+++ api/controllers/Post.controller.js
@@ -1,4 +1,7 @@
 import sharp from "sharp";
+import cloudinary from "../utils/cloudinary.js";
+import Post from "../model/Post.js"
+import User from "../model/User.js";
 
 export const addNewPost = async (req, res) => {
     try {
@@ -21,8 +24,23 @@ export const addNewPost = async (req, res) => {
         //converting image to data uri
         const fileUri= `data:image/jpeg;base64,${optimizedImageBuffer.toString('base64')}`;
         console.log("fileUri", fileUri)
-
-
+        const cloudResponse = await cloudinary.uploader.upload(fileUri);
+        const post = await Post.create({
+            caption,
+            image: cloudResponse.secure_url,
+            author: authorId
+        })
+        // Now we need to push this post to User.model.js to posts section as it is referenced to Post schema
+        const user = await User.findById(authorId);
+        if(user){
+            user.posts.push(post._id);// pushing the id of the post in the posts array of the user.model schema and then further if you want you can populate the posts and get the full post info
+            await user.save();
+        }
+        await post.populate({path: 'author', select:'-password'})
+        return res.status(201).json({
+            message: "New Post added!",
+            post
+        })
     } catch (error) {
         console.log("Error adding the Post", error);
     }
--- api/model/Post.js
+++ api/model/Post.js
@@ -10,4 +10,4 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model("Post", postSchema);
 
-module.exports = Post
+export default Post;
